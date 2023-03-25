17fca037b2aa739e171c4ab8633a77e3240a698e
Renato <renato.printf@gmail.com>
2023-03-25 20:27:28 -0300

    method DELETE created

--- routes/accounts.js
+++ routes/accounts.js
@@ -46,4 +46,18 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+//DELETE
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      account => account.id !== parseInt(req.params.id)
+    )
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.end();
+  } catch (error) {
+    res.status(400).send({ error: err.message });
+  }
+})
+
 export default router;
