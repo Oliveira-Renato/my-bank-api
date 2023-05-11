f7179d790cdc5c5fe8f38639d223633b3edbd154
Renato <renato.printf@gmail.com>
2023-05-10 21:35:08 -0300

    changing to GraphQL

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/account', accountsRouter);
 
 
